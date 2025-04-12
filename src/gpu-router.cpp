cfe1ecde4d19f0f3c4901e41fe649f087686e51c
Matei Mantu <matei.mantu1@gmail.com>
2025-04-12 13:07:25 +0000

    stuff

--- src/gpu-router.cpp
+++ src/gpu-router.cpp
@@ -11,50 +11,44 @@
 #include <tbb/flow_graph.h>
 #include "dpc_common.hpp"
 #include "protocols.h"
+#include <arpa/inet.h>
 
 constexpr size_t BURST_SIZE = 32;
 constexpr size_t PACKET_SIZE = 1519;
 
 using Packet = std::array<uint8_t, PACKET_SIZE>;
 using PacketBatch = std::vector<Packet>;
+using ArrayCounter = std::array<u_int8_t, BURST_SIZE>;
+using InspectResult = std::pair<PacketBatch, std::array<ArrayCounter, 6>>;
 
 /**
  * Parse the packet and determine its type
  * returns protocol index
  */
-int parse_packet(const char* packet, int idx) {
+int parse_packet(const unsigned char* packet) {
     struct ether_header* eth_hdr = (struct ether_header*) packet;
-
+    struct iphdr* ip_hdr = (struct iphdr*) (packet + sizeof(struct ether_header));
     switch (ntohs(eth_hdr->ether_type)) {
         case ETHERTYPE_IP:
-            struct iphdr* ip_hdr = (struct iphdr*) (packet + sizeof(struct ether_header));
-            std::cout << "IP packet" << std::endl;
-            switch (ntoh(ip_hdr->protocol)) {
+            switch (ip_hdr->protocol) {
                 case IPPROTO_ICMP:
                     return ICMP_IDX;
                 case IPPROTO_TCP:
-                    std::cout << "TCP packet" << std::endl;
                     return TCP_IDX;
                 case IPPROTO_UDP:
-                    std::cout << "UDP packet" << std::endl;
                     return UDP_IDX;
                 default:
-                    std::cout << "Unknown protocol" << std::endl;
                     return -1;
             }
         case ETHERTYPE_ARP:
-            std::cout << "ARP packet" << std::endl;
             return ARP_IDX;
         case ETHERTYPE_IPV6:
-            std::cout << "IPv6 packet" << std::endl;
             return IPv6_IDX;
         default:
-            std::cout << "Unknown Ethernet type" << std::endl;
             return -1;
     }
 }
 
-int main(int argc, char* argv[]) {
 int main(int argc, char* argv[]) {
     sycl::queue q;
 
@@ -64,7 +58,13 @@ int main(int argc, char* argv[]) {
     char errbuf[PCAP_ERRBUF_SIZE];
     int nth = 10;  // number of threads
     auto mp = tbb::global_control::max_allowed_parallelism;
-    pcap_t* handle = pcap_open_offline("/root/keysight-challenge2025/src/capture2.pcap", errbuf);
+    pcap_t *handle{};
+
+    if (argc == 1) {
+        handle = pcap_open_offline("/root/keysight-challenge2025/src/capture2.pcap", errbuf);
+    } else {
+        // handle = pcap_open_live(argv[1], , , ,errbuf);
+    }
     tbb::global_control gc(mp, nth);
     tbb::flow::graph g;
 
@@ -92,63 +92,69 @@ int main(int argc, char* argv[]) {
         }
     };
 
-    std::array<std::array<u_int8_t, burst_size>, 6> counters;
     // Packet inspection node
-    tbb::flow::function_node<PacketBatch> inspect_packet_node {
-        g, tbb::flow::unlimited, [&](PacketBatch batch) -> PacketBatch {
-            PacketBatch result;
+    tbb::flow::function_node<PacketBatch, PacketBatch> inspect_packet_node {
+        g, tbb::flow::unlimited, [&](PacketBatch batch) -> InspectResult {
+            PacketBatch result(batch.size());
+            std::array<std::array<u_int8_t, BURST_SIZE>, 6> counters{std::array<u_int8_t, BURST_SIZE>{}};
 
             // By including all the SYCL work in a {} block, we ensure
             // all SYCL tasks must complete before exiting the block
             {
                 // Create a SYCL buffer from the counters array
                 sycl::buffer<u_int8_t, 2> counters_buf(reinterpret_cast<u_int8_t*>(counters.data()),
-                                                       sycl::range<2>(6, burst_size));
+                                                       sycl::range<2>(6, BURST_SIZE));
 
-                                                       sycl::queue gpuQ(sycl::gpu_selector_v, dpc_common::exception_handler);
-
-                std::cout << "Selected GPU Device Name: " <<
-                gpuQ.get_device().get_info<sycl::info::device::name>() << "\n";
+                                                       sycl::queue gpuQ(sycl::default_selector_v, dpc_common::exception_handler);
                 
                 sycl::range<1> n_items{batch.size()}; 
                 sycl::buffer batch_buffer(batch);
                 sycl::buffer result_buffer(result);
 
                 gpuQ.submit([&](sycl::handler& h) {
-                    auto counters_acc = counters_buf.get_access<sycl::access::mode::read_write>(h);
-                    auto batch_acc = batch_buffer.get_access<sycl::access::mode::read>(h);
-                    auto result_acc = result_buffer.get_access<sycl::access::mode::write>(h);
-                    
+                    sycl::accessor counters_acc(counters_buf, h, sycl::read_write);
+                    sycl::accessor batch_acc(batch_buffer, h, sycl::read_only);
+                    sycl::accessor result_acc(result_buffer, h, sycl::write_only);
+
+                    sycl::stream out(1024, 256, h); // buffer size, message size
                     auto compute = [=](auto i) {
                         // Process the packets
-                        char* packet = batch_acc[i];
-                        uint8_t packet_type = parse_packet(packet);
+                        const unsigned char* packet = batch_acc[i].data();
+                        int8_t packet_type = parse_packet(packet);
+                        // out << "Packet " << i[0] << " type: " << packet_type << ".\n";
                         if (packet_type != -1) {
                             if (packet_type == ICMP_IDX || 
                                 packet_type == TCP_IDX ||
                                 packet_type == UDP_IDX) {
                                 counters_acc[IPv4_IDX][i]++;
-                                result_acc[i] = packet;
                             }
+                            result_acc[i] = batch_acc[i];
                             counters_acc[packet_type][i]++;
                         }
                     };
-
-                    h.parallel_for(nr_packets, compute);
+                    h.parallel_for(n_items, compute);
                 }).wait_and_throw();  // end of the commands for the SYCL queue
+                std::cout << "SYCL block done\n";
+                for (int i = 0; i < 6; ++i) {
+                    std::cout << "Counter " << i << ": ";
+                    for (int j = 0; j < BURST_SIZE; ++j) {
+                        std::cout << static_cast<int>(counters[i][j]) << " ";
+                    }
+                    std::cout << std::endl;
+                }
             }  // End of the scope for SYCL code; the queue has completed the work
             // Return the number of packets processed
-            return result;
+            
+            return {result, counters};
         }
     };
 
     tbb::flow::function_node<PacketBatch, PacketBatch> routing_node{
         g, tbb::flow::unlimited,
-        [&](const PacketBatch& batch) -> PacketBatch {
+        [&](const InspectResult& inspect_result) -> PacketBatch {
+            auto batch = inspect_result.first;
             {
                 sycl::queue gpuQ(sycl::default_selector_v, dpc_common::exception_handler);
-                std::cout << "Selected Device Name: " <<
-                gpuQ.get_device().get_info<sycl::info::device::name>() << "\n";
 
                 sycl::range<1> n_items{batch.size()}; 
                 sycl::buffer batch_buffer(batch);
@@ -157,6 +163,9 @@ int main(int argc, char* argv[]) {
                     auto compute = [=](sycl::id<1> index) {
                         auto packet = batch_accessor[index];
                         ether_header* eth = (ether_header*)packet.data();
+                        if (eth->ether_type == ETHERTYPE_IP) {
+                            return;
+                        }
                         iphdr* ip = (iphdr*)(packet.data() + sizeof(ether_header));
 
                         ip->daddr += 1 + (1 << 8) + (1 << 16) + (1 << 24); // Increment destination IP
@@ -182,31 +191,26 @@ int main(int argc, char* argv[]) {
 
     // construct graph
     // Sum counters node
-    // tbb::flow::function_node<int, std::array<int, 6>> sum_counters_node {
-    //     g, tbb::flow::unlimited, [&](int) {
-    //         std::array<int, 6> total_counters = {0, 0, 0, 0, 0, 0};
-
+    tbb::flow::function_node<int, std::array<int, 6>> sum_counters_node {
+        g, tbb::flow::unlimited, [&](const InspectResult &result) {
+            std::array<int, 6> total_counters = {0, 0, 0, 0, 0, 0};
             
-            
-    //         return total_counters;
-    //     }
-    // };
+            // TODO: Parallel reduce
+            for (const auto &c : total_counters) {
+                std::cout << c << ' ';
+            }
+            std::cout << '\n';
+        }
+    };
     
     // construct graph
     tbb::flow::make_edge<PacketBatch>(in_node, inspect_packet_node);
     tbb::flow::make_edge<PacketBatch>(inspect_packet_node, routing_node);
+    tbb::flow::make_edge<PacketBatch>(inspect_packet_node, sum_counters_node);
     tbb::flow::make_edge<PacketBatch>(routing_node, send_node);
 
     in_node.activate();
     g.wait_for_all();
 
     std::cout << "Done waiting" << std::endl;
-    // Print counters array
-    for (int i = 0; i < 6; ++i) {
-        std::cout << "Counter " << i << ": ";
-        for (int j = 0; j < burst_size; ++j) {
-            std::cout << static_cast<int>(counters[i][j]) << " ";
-        }
-        std::cout << std::endl;
-    }
 }
